e0ed901ff0f3bc5cad9011d0688a3f5870f223ee
ngomezap <igomez.ap@gmail.com>
2022-08-08 18:36:41 +0200

    Add the editing option

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Overview } from './components/Overview';
+import './App.css'
 
 
 class App extends React.Component {
@@ -9,6 +10,8 @@ class App extends React.Component {
     this.state = {tasks: []};
     this.onButtonClicked = this.onButtonClicked.bind(this);
     this.onDeleteButton = this.onDeleteButton.bind(this);
+    this.onEditButton = this.onEditButton.bind(this);
+    this.onSubmitButton = this.onEditButton.bind(this);
   }
 
   onButtonClicked(taskArr){
@@ -23,11 +26,24 @@ class App extends React.Component {
     })
   }
 
+  onEditButton(taskArr){
+    this.setState({
+      tasks: taskArr
+    });
+  }
+
+  onSubmitButton(taskArr){
+    this.setState({
+      tasks: taskArr
+    });
+  }
+
   render(){
     return (
       <Overview 
       onClick = {this.onButtonClicked}
-      onDelete = {this.onDeleteButton} 
+      onDelete = {this.onDeleteButton}
+      onEdit = {this.onEditButton} 
       tasks={this.state.tasks}/>
     );
   }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -7,11 +7,17 @@ export class Overview extends React.Component{
         super(props);
         this.onButtonClicked = this.onButtonClicked.bind(this);
         this.onDeleteButton = this.onDeleteButton.bind(this);
+        this.onEditButton = this.onEditButton.bind(this);
+        this.onSubmitButton = this.onSubmitButton.bind(this);
     }
 
     onButtonClicked(){
         let arr = this.props.tasks;
-        arr.push({name: document.querySelector('input').value, id: uniqid()});
+        arr.push({
+            name: document.querySelector('input').value,
+            id: uniqid(),
+            editmode: 'off'
+        });
         
         this.props.onClick(arr);
     }
@@ -26,12 +32,55 @@ export class Overview extends React.Component{
         this.props.onDelete(arr);
     }
 
+    onEditButton(e){
+        let arr = this.props.tasks;
+        let id = e.target.parentNode.id;
+        arr = arr.map((tk) => {
+            if(tk.id === id)
+                tk.editmode = 'on';
+            return tk;
+        })
+
+        this.props.onEdit(arr);
+    }
+
+    onSubmitButton(e){
+        let arr = this.props.tasks;
+        let id = e.target.parentNode.id;
+        let input = document.querySelector(`#${id} > input`);
+        
+        arr = arr.map((tk) => {
+            if(tk.id === id){
+                tk.editmode = 'off';
+                tk.name = input.value;
+            }
+            return tk;
+        })
+
+        this.props.onEdit(arr);
+    }
+    
+
     render(){
 
         let rows = [];
         console.log(uniqid())
         this.props.tasks.forEach((tk) => {
-            rows.push(<li id={tk.id} key={tk.id}>{tk.name} <button onClick={this.onDeleteButton}>Borrar</button></li>);
+            if(tk.editmode === 'off'){
+                rows.push(
+                    <li id={tk.id} key={tk.id}>
+                        <p>{tk.name}</p> 
+                        <button onClick={this.onDeleteButton}>Borrar</button>
+                        <button onClick={this.onEditButton}>Editar</button>
+                    </li>);
+            }else{
+                rows.push(
+                    <li id={tk.id} key={tk.id}>
+                        <input type="text" defaultValue={tk.name}/> 
+                        <button onClick={this.onDeleteButton}>Borrar</button>
+                        <button onClick={this.onSubmitButton}>Submit</button>
+                    </li>);
+            }
         })
 
         
